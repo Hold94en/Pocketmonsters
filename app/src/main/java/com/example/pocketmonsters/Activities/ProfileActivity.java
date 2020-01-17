d3ddb79da196ad27aaa44565eb0bda0d37c07223
Hold94en <fra.anania94@gmail.com>
2020-01-17 12:35:00 +0100

    Add fade-in and fade-out animations to rankings and close buttons when fragment enters / exits

--- app/src/main/java/com/example/pocketmonsters/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/pocketmonsters/Activities/ProfileActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -99,8 +101,15 @@ public class ProfileActivity extends AppCompatActivity implements FragmentEditPr
         if (myFragment == null) {
             fragmentTransaction.replace(R.id.fragment_container, fragment, tag);
             fragmentTransaction.commitAllowingStateLoss();
+
+            buttonRankings.animate().alpha(0.0f).setDuration(100);
+            buttonClose.animate().alpha(0.0f).setDuration(100);
+
         } else {
             fragmentTransaction.remove(myFragment).commit();
+
+            buttonRankings.animate().alpha(1.0f).setDuration(100);
+            buttonClose.animate().alpha(1.0f).setDuration(100);
         }
 
     }
